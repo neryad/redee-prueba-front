9f913394358b7d3d107d846e7f06cb51ed75e152
Neryad <dayern.gomez@hotmail.com>
2025-04-26 14:35:51 -0400

    feat: refactor manage company component with updated HTML structure and router imports

--- src/app/home/home-page.component.ts
+++ src/app/home/home-page.component.ts
@@ -3,7 +3,7 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterLink],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css',
 })
--- src/app/manage-company/manage-company.component.ts
+++ src/app/manage-company/manage-company.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-manage-company',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './manage-company.component.html',
-  styleUrl: './manage-company.component.css'
+  styleUrl: './manage-company.component.css',
 })
-export class ManageCompanyComponent {
-
-}
+export class ManageCompanyComponent {}
