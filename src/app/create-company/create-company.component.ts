9d761977c0bf6824dbc7350630aac041e5ed444e
Neryad <dayern.gomez@hotmail.com>
2025-04-26 14:09:34 -0400

    feat: implement create company form with validation and layout

--- src/app/create-company/create-company.component.ts
+++ src/app/create-company/create-company.component.ts
@@ -1,11 +1,41 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-create-company',
-  imports: [],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './create-company.component.html',
-  styleUrl: './create-company.component.css'
+  styleUrl: './create-company.component.css',
 })
 export class CreateCompanyComponent {
+  companyForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.companyForm = this.fb.group({
+      rnc: ['', Validators.required],
+      companyName: ['', Validators.required],
+      comercialName: ['', Validators.required],
+      category: [''],
+      paymentSchema: ['', Validators.required],
+      status: ['', Validators.required],
+      activityEconomics: ['', Validators.required],
+      governanceBranch: ['', Validators.required],
+    });
+  }
+  onSubmit() {
+    if (this.companyForm.valid) {
+      console.log('Form Submitted!', this.companyForm.value);
+      // Aquí puedes agregar la lógica para enviar el formulario a tu API o servicio
+    } else {
+      console.log('Form is invalid');
+    }
+  }
+  onCancel() {
+    this.companyForm.reset();
+    console.log('Form reset');
+
+    // this.router.navigate(['/home']);
+  }
 }
