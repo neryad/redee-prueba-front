7120d54fcbf8fe07c9a9737c59ac127af5dbecdd
Neryad <dayern.gomez@hotmail.com>
2025-04-26 13:31:15 -0400

    feat: add home page component with routing and initial layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,11 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: '',
+    loadComponent: () =>
+      import('./home/home-page.component').then((m) => m.HomePageComponent),
+  },
+  {
     path: 'create',
     loadComponent: () =>
       import('./create-company/create-company.component').then(
